5ae92591ac0f9a377159e412b2289d540b1997ca
emmacastiblanco <ecastiblan@gmail.com>
2023-12-08 08:10:31 -0700

    I hope this works

---
+++ setup.R
@@ -0,0 +1,14 @@
+# set up  script 
+
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+
+packageLoad(c("tidyverse", "palmerpenguins"))
